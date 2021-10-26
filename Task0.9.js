8f4312e28cf10c80315caf7316f68fe45c457216
Joel612 <77391079+Joel612@users.noreply.github.com>
2021-10-26 15:44:22 +0200

    Update Task0.9.js

--- Task0.9.js
+++ Task0.9.js
@@ -1,13 +1,11 @@
-function findingVowels(str) {
-  const vowelsArray = ["a", "e", "i", "o", "u"];
-  const presentvowel = [];
-
-  for (let i = 0; i < vowelsArray.length; i++) {
-    if (str.toLowerCase().includes(vowelsArray[i])) {
-      presentvowel.push(vowelsArray[i]);
+function findingVowels(str){
+    let vowels = "aeuoi";
+    let vowelsFound = "";
+    for(let i = 0; i < vowels.length; i++){
+        if(str.toLowerCase().includes(vowels[i])){
+          vowelsFound += vowels[i] + " ";
+        }
     }
-  }
-  console.log("Vowels:",presentvowel);
+    console.log("Vowels:", vowelsFound);
 }
-
-findingVowels("UmuziUmuzi");;
+findingVowels("UmuziUmuzi");
