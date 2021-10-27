8323cef6c1bd6a487046bd205b213e48fd905f98
Joel612 <77391079+Joel612@users.noreply.github.com>
2021-10-27 15:14:02 +0200

    Update Task0.9.js

--- Task0.9.js
+++ Task0.9.js
@@ -3,7 +3,7 @@ function findingVowels(str){
     let vowelsFound = "";
     for(let i = 0; i < vowels.length; i++){
         if(str.toLowerCase().includes(vowels[i])){
-          vowelsFound += vowels[i] + " ";
+          vowelsFound += vowels[i] + ", ";
         }
     }
     console.log("Vowels:", vowelsFound);
